bd0500204b8d793276e4bef8be541fc5174aa4b3
Fatih Koç <mail@fatihkoc.net>
2023-04-18 15:38:21 +0300

    add port to e2e tests

--- e2e/cypress/e2e/health.cy.js
+++ e2e/cypress/e2e/health.cy.js
@@ -1,6 +1,6 @@
 describe('response status is 200', () => {
     it('GET', () => {
-        cy.request('GET', 'http://34.201.47.19')
+        cy.request('GET', 'http://34.201.47.19:30000')
         .then((res) => {expect(res).to.have.property('status', 200)
         })        
     })
@@ -8,7 +8,7 @@ describe('response status is 200', () => {
 
 describe('response body is not null', () => {
     it('GET', () => {
-        cy.request('GET', 'http://34.201.47.19')
+        cy.request('GET', 'http://34.201.47.19:30000')
         .then((res) => {expect(res.body).to.not.be.null
         })        
     })
