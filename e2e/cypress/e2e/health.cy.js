4ca981b97738e28b5b7513ae2d782af6d4276ea4
Fatih Koç <mail@fatihkoc.net>
2023-04-18 18:55:05 +0300

    finalize

--- e2e/cypress/e2e/health.cy.js
+++ e2e/cypress/e2e/health.cy.js
@@ -1,6 +1,6 @@
 describe('response status is 200', () => {
     it('GET', () => {
-        cy.request('GET', 'http://44.210.226.212:30000')
+        cy.request('GET', 'http://3.80.142.57:30000')
         .then((res) => {expect(res).to.have.property('status', 200)
         })        
     })
@@ -8,7 +8,7 @@ describe('response status is 200', () => {
 
 describe('response body is not null', () => {
     it('GET', () => {
-        cy.request('GET', 'http://44.210.226.212:30000')
+        cy.request('GET', 'http://3.80.142.57:30000')
         .then((res) => {expect(res.body).to.not.be.null
         })        
     })
